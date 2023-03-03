a20b767491a3c70a739cc086a0296799a5ae99eb
VishnuPriya-Kesavan <118333180+VishnuPriya-Kesavan@users.noreply.github.com>
2023-03-03 04:23:27 +0000

    Update total.sh

--- total.sh
+++ total.sh
@@ -1,11 +1,10 @@
 # Write a shell program to add the first 10 multiples of 3
 #!/bin/bash
 sum=0
-while [$
+a=0
+while [$a -lt 10]
 do
-  num=$((3*$i))
+  num=$((3*$a))
   sum=$((sum + num))
 done
 echo $sum
-
-
