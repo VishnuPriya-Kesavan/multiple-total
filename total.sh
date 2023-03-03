545d6fa465b838ea48f9a82b7801f0436c102c08
VishnuPriya-Kesavan <118333180+VishnuPriya-Kesavan@users.noreply.github.com>
2023-03-03 04:09:48 +0000

    Update total.sh

--- total.sh
+++ total.sh
@@ -1,8 +1,10 @@
 # Write a shell program to add the first 10 multiples of 3
 #!/bin/bash
-$sum=0
-for i in {1..10..3};
+sum=0
+for i in {1..10};
 do
-  sum=$(($sum + $i))
+  num=$((3*$i))
+  sum=$((sum + num))
 done
-echo $sum
+echo "The sum of first 10 multiples of 3 is $sum"
+
